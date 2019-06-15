9af41762979c447c6c0962e222a7a9ecb88fdf8d
yuanyulou <434356430@qq.com>
2019-06-15 14:11:09 +0800

    Initial commit

---
+++ src/main/java/cn/itlou/dubbostudy/DubbostudyApplication.java
@@ -0,0 +1,13 @@
+package cn.itlou.dubbostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubbostudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubbostudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/itlou/dubbostudy/DubbostudyApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.itlou.dubbostudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DubbostudyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
